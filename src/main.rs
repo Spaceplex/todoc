a19feda3dd165addc6f392317f1cf4639cf380b4
Spaceplex <spaecplex@gmail.com>
2026-06-10 00:22:42 -0400

    it works

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, fs::File, hash::Hash};
+use std::{collections::HashSet, fs::File, io::{Seek, SeekFrom}};
 
 use colored::Colorize;
 
@@ -11,46 +11,90 @@ const CHECK_MARK: &str = "\u{2713}"; // ✓ (Check Mark)
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = std::env::args().collect();
 
-    let mut path = dirs::data_dir().unwrap();
-    path.push("/todac/tasks.json");
+    let mut path = dirs::data_dir().ok_or("Could not find data directory")?;
+    path.push("todac");
+    std::fs::create_dir_all(&path)?;
+    path.push("tasks.json");
 
-    let file = File::open(path)?;
-    let tasks: Vec<task::Task> = serde_json::from_reader(file)?;
-
-    // println!("{} {} !", "it".green(), "works".blue().bold());
-    // println!("{}", EMPTY_BOX);
+    let mut file = std::fs::OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(&path)?;
+    let mut tasks: Vec<task::Task> = if file.metadata()?.len() == 0 {
+        vec![]
+    } else {
+        serde_json::from_reader(&file)?
+    };
 
     match args.len() {
-        1 => output_list(&tasks),
-        2 => println!("more args"),
+        1 => {
+            output_list(&tasks);
+            return Ok(());
+        },
+        2 => {
+            println!("more args");
+            return Ok(());
+        }
         _ => {
             let first_cmd = args[1].as_str();
             let rest = &args[2..];
 
-            if !has_duplicates(rest){
+            if has_duplicates(rest){
                 println!("Duplicate input detected");
             }
 
             match first_cmd {
                 "add" => {
                     for task in rest {
-                        add_task(task.to_string());
+                        add_task(task.to_string(), &mut tasks);
                     }
                 },
-                _ => println!("invalid command")
+                "done" => {
+                    let r: Result<Vec<usize>, _> = rest.iter().map(|n| n.parse::<usize>()).collect();
+                    let r = r.map_err(|_| "All inputs must be numbers for \"done\"")?;
+                    if r.iter().any(|&n| n < 1 || n > tasks.len()) {
+                        return Err(String::from("Out of bound number passed").into());
+                    }
+                    for n in r {
+                        tasks[n - 1].done = true;
+                    }
+                }
+                "delete" => {
+                    let r: Result<Vec<usize>, _> = rest.iter().map(|n| n.parse::<usize>()).collect();
+                    let r = r.map_err(|_| "All inputs must be numbers for \"done\"")?;
+                    let mut s = r.clone();
+                    s.sort_by(|a,b| b.cmp(a));
+                    let s = s;
+                    if s.iter().any(|&n| n < 1 || n > tasks.len()) {
+                        return Err(String::from("Out of bound number passed").into());
+                    }
+                    for n in s {
+                        tasks.remove(n - 1);
+                    }
+                }
+                _ => {
+                    println!("invalid command");
+                    return Ok(());
+                }
             }
         }
     }
+
+    file.set_len(0)?;
+    file.seek(SeekFrom::Start(0))?;
+    serde_json::to_writer_pretty(&file, &tasks)?;
 
     output_list(&tasks);
     Ok(())
 }
 
-fn add_task(title: String){
+fn add_task(title: String, tasks: &mut Vec<Task>){
     let task = Task {
         title,
         done: false
     };
+    tasks.push(task);
 }
 
 fn output_list(tasks: &[task::Task]){
@@ -58,14 +102,12 @@ fn output_list(tasks: &[task::Task]){
         println!("Tasks empty");
         return;
     }
-    let mut incrementer = 1;
-    for task in tasks {
-        match task.done {
-            true => println!("{}. {} {}", incrementer, CHECK_MARK.green(), task.title.as_str().on_cyan()),
-            false => println!("{}. {} {}", incrementer, EMPTY_BOX.blue(), task.title),
+    tasks.iter().enumerate().for_each(|tsk| {
+        match tsk.1.done {
+            true => println!("{}. {} {}", tsk.0 + 1, CHECK_MARK.green(), tsk.1.title.as_str().truecolor(150, 150, 150)),
+            false => println!("{}. {} {}", tsk.0 + 1, EMPTY_BOX.blue(), tsk.1.title),
         }
-        incrementer += 1;
-    }
+    });
 }
 
 fn has_duplicates<I>(iter: I) -> bool
